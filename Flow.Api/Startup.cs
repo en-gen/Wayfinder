1819356acd239cbd5e995ea9275b10b2b08e1ae8
Brian Engen <engenb@gmail.com>
2019-03-12 22:32:41 -0500

    api and silo projects started

---
+++ Flow.Api/Startup.cs
@@ -0,0 +1,63 @@
+﻿using Flow.Api.Infrastructure.Swagger;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Flow.Api
+{
+    public class Startup
+    {
+        private IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddMvcCore();
+
+            services
+                .AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            
+            services
+                .AddApiVersioning()
+                .AddVersionedApiExplorer(options => options.GroupNameFormat = "'v'VVV")
+
+                .AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>()
+                .AddSwaggerGen();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApiVersionDescriptionProvider versions)
+        {
+            app
+                .UseResponseCompression()
+                .UseResponseCompression()
+                .UseSwagger(opts => opts.RouteTemplate = "swagger/flow/{documentName}/swagger.json")
+                .UseSwaggerUI(opts =>
+                {
+                    opts.RoutePrefix = "explorer/flow";
+                    opts.DisplayRequestDuration();
+
+                    foreach (var desc in versions.ApiVersionDescriptions)
+                    {
+                        opts.SwaggerEndpoint(
+                            $"/swagger/flow/{desc.GroupName}/swagger.json",
+                            $"Flow {desc.GroupName}");
+                    }
+                })
+                .UseMvc();
+        }
+    }
+}
---
+++ Flow.Silo/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using AutoMapper;
+using Flow.Grains.Infrastructure.AutoMapper;
+using Flow.Grains.Infrastructure.Extensions;
+using Flow.Grains.Infrastructure.Quartz;
+using Flow.Grains.Interfaces.Plan.Case;
+using Flow.Grains.Plan.PlanItem;
+using Flow.Grains.Services.PlanItemBehaviorConfigurator;
+using Flow.Grains.Services.PlanItemStateMachineConfigurator;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NodaTime;
+using NodaTime.Extensions;
+using Orleans;
+using Orleans.Configuration;
+using Orleans.Hosting;
+using Orleans.Runtime;
+using Serilog;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Exceptions;
+
+namespace Flow.Silo
+{
+    public class Program
+    {
+        private static ISiloHost SiloHost { get; set; }
+
+        public async Task Main()
+        {
+            var levelSwitch = new LoggingLevelSwitch
+            {
+                MinimumLevel = LogEventLevel.Debug
+            };
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.ControlledBy(levelSwitch)
+                .Enrich.FromLogContext()
+                .Enrich.WithExceptionDetails()
+                .WriteTo.Seq(
+                    "http://localhost:5341",
+                    controlLevelSwitch: levelSwitch
+                )
+                .CreateLogger();
+
+            Log.Logger.Debug("initializing silo");
+
+            try
+            {
+                SiloHost = new SiloHostBuilder()
+                    .ConfigureLogging(builder => builder.AddSerilog(Log.Logger))
+                    .ConfigureServices(ConfigureServices)
+                    
+                    .UseAzureStorageClustering(options => options.ConnectionString = "UseDevelopmentStorage=true")
+                    // .UseLocalhostClustering()
+
+                    // .AddMemoryGrainStorageAsDefault()
+                    .AddAzureTableGrainStorageAsDefault() // grain state
+                    .AddLogStorageBasedLogConsistencyProvider() // journaled grain
+
+                    .AddAzureTableGrainStorage("PubSubStore")
+                    // .AddMemoryGrainStorage("PubSubStore") // stream storage
+                    
+                    // .AddAzureQueueStreams("Default", options => options.)
+                    .AddSimpleMessageStreamProvider("Default", options => options.FireAndForgetDelivery = true) // cluster stream provider
+                    // .UseInMemoryReminderService()
+                    .UseAzureTableReminderService("UseDevelopmentStorage=true")
+
+                    .ConfigureApplicationParts(parts => parts
+                        .AddApplicationPart(typeof(IPlanItemGrain).Assembly) // Flow.Grains
+                        .AddApplicationPart(typeof(ICaseGrain).Assembly)) //Flow.Grains.Interfaces
+
+                    .UseSiloUnobservedExceptionsHandler()
+
+                    .Build();
+
+                await SiloHost.StartAsync();
+            }
+            catch (Exception ex)
+            {
+                Log.Logger.Error(ex, "failure during cluster startup");
+            }
+        }
+
+
+        private static void ConfigureServices(HostBuilderContext ctx, IServiceCollection services)
+        {
+            services
+                //.AddSingleton(sp => new ClientBuilder()
+                //    .UseLocalhostClustering()
+
+                //    .AddSimpleMessageStreamProvider("Default")
+
+                //    .ConfigureApplicationParts(parts => parts
+                //        .AddApplicationPart(typeof(IPlanItemGrain).Assembly)
+                //        .AddApplicationPart(typeof(ICaseGrain).Assembly))
+
+                //    .Build())
+                .AddSingleton<IClock>(SystemClock.Instance.InUtc())
+                .AddRuleExecutor()
+                .AddSingleton<IPlanItemBehaviorConfigurator, PlanItemBehaviorConfiguratorService>()
+                .AddSingleton<IPlanItemStateMachineConfigurator, PlanItemStateMachineConfiguratorService>()
+                .AddAutoMapper(cfg => cfg.AddProfile<CaseFlowProfile>(), Enumerable.Empty<Assembly>())
+                .AddQuartz(QuartzSchedulerConfig.Volatile);
+        }
+    }
+}
